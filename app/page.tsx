c2db95d235dacf1300d2c09d9326414a268c59d9
seba-sanchis <seba.sanchis@gmail.com>
2024-02-13 16:56:33 -0300

    live cursors

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+
+import Live from "@/components/Live";
+
 export default function Page() {
   return (
-    <div className="h-screen w-full flex justify-center items-center text-center">
-      <h1 className="text-2xl text-white">Liveblocks</h1>
+    <div>
+      <Live />
     </div>
   );
 }
