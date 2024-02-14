5c05fc3c9dd938c5b10ec404180fe4736ba47369
seba-sanchis <seba.sanchis@gmail.com>
2024-02-14 19:24:40 -0300

    free drawing fix

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ import {
   handleCanvasObjectScaling,
   handleCanvasSelectionCreated,
   handleCanvaseMouseMove,
+  handlePathCreated,
   handleResize,
   initializeFabric,
   renderCanvas,
@@ -170,6 +171,13 @@ export default function Page() {
       handleCanvasObjectScaling({
         options,
         setElementAttributes,
+      });
+    });
+
+    canvas.on("path:created", (options) => {
+      handlePathCreated({
+        options,
+        syncShapeInStorage,
       });
     });
 
