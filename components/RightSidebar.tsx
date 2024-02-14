ce2b490e56d5d1c4b025188644d030628c283bca
seba-sanchis <seba.sanchis@gmail.com>
2024-02-14 16:34:19 -0300

    right sidebar

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,7 +1,29 @@
-export default function RightSidebar() {
+import { RightSidebarProps } from "@/types/type";
+import Color from "./settings/Color";
+import Dimensions from "./settings/Dimensions";
+import Export from "./settings/Export";
+import Text from "./settings/Text";
+
+export default function RightSidebar({
+  elementAttributes,
+  setElementAttributes,
+  fabricRef,
+  isEditingRef,
+  activeObjectRef,
+  syncShapeInStorage,
+}: RightSidebarProps) {
   return (
-    <section className="flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 min-w-[227px] sticky left-0 h-full max-sm:hidden select-none overflow-y-auto pb-20">
+    <section className="flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 min-w-[227px] sticky right-0 h-full max-sm:hidden select-none">
       <h3 className="px-5 pt-4 text-xs uppercase">Design</h3>
+      <span className="text-xs text-primary-grey-300 mt-3 px-5 border-b border-primary-grey-200 pb-4">
+        Make changes to canvas as you like
+      </span>
+
+      <Dimensions />
+      <Text />
+      <Color />
+      <Color />
+      <Export />
     </section>
   );
 }
